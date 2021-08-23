e035e06d86adbc882f840db235be8c33aa68c28b
Mathieu Bastien Monney <mamonney@cisco.com>
2021-08-23 09:27:22 +0200

    Fix ampere

--- custom_components/kostal/const.py
+++ custom_components/kostal/const.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 from homeassistant.const import (
     POWER_WATT,
     ELECTRIC_POTENTIAL_VOLT,
-    ELECTRICAL_CURRENT_AMPERE,
+    ELECTRIC_CURRENT_AMPERE,
 )
 
 DOMAIN = "kostal"
@@ -16,5 +16,5 @@ MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=1)
 SENSOR_TYPES = {
     "solar_generator_power": ["Solar generator power", POWER_WATT, "mdi:solar-power"],
     "ac_voltage": ["AC Voltage", ELECTRIC_POTENTIAL_VOLT, "mdi:current-ac"],
-    "ac_current": ["AC Current", ELECTRICAL_CURRENT_AMPERE, "mdi:flash"],
+    "ac_current": ["AC Current", ELECTRIC_CURRENT_AMPERE, "mdi:flash"],
 }
